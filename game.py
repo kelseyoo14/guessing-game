78f048564b8fea025c3ff353a3591c5ba6788f32
Hackbright Student <info@hackbrightacademy.com>
2016-04-05 15:30:29 -0700

    initial commit

---
+++ game.py
@@ -0,0 +1,34 @@
+# Put your code here
+import random
+
+name = raw_input("What is your name? ")
+while True:
+    try:
+        guess = int(raw_input("%s, I'm thinking of a number between 1 and 100. Try to guess my number. " % name))
+        break
+    except ValueError:
+        print("I'm not sure that was a number. Try again: ")
+
+
+#generate random number
+random_number = random.randint(1, 100)
+print random_number
+count = 1
+
+#loop
+while guess != random_number:
+    if random_number > guess:
+        print "too low"
+    else: 
+        print "too high"
+    while True:
+        try:
+            guess = int(raw_input("Guess again: "))
+            break
+        except ValueError:
+            print("I'm not sure that was a number. Try again: ")
+    count += 1
+    # print count
+
+
+print "Well done, %s! You found my number in %s trys." % (name, count)
